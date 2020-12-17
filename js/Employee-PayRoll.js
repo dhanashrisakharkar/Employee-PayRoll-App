af50c37aeae2366a84eff8bcb22ff41f3a65dc16
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-12-18 01:48:20 +0530

    [Dhanashri] Add. Storage data locally while convert into string

--- js/Employee-PayRoll.js
+++ js/Employee-PayRoll.js
@@ -68,6 +68,26 @@ const getInputValueById = (id) => {
 }
 
 const getInputElementValue = (id) => {
-    let value = document.getElementById(id).value;
-    return value;
+        let value = document.getElementById(id).value;
+        return value;
+    }
+    //Use Case 12
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData)
+    } catch (e) {
+        return;
+    }
+}
+
+function createAndUpdateStorage(employeePayrollData) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if (employeePayrollList != undefined) {
+        employeePayrollList.push(employeePayrollData);
+    } else {
+        employeePayrollList = [employeePayrollData]
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
