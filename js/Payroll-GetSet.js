9dd4bc53484f00c79955b7d0a809bb80e3499d16
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-12-24 17:22:30 +0530

    [Dhanahsri] Refactor. Updated Getter Setter file objects has been added

--- js/Payroll-GetSet.js
+++ js/Payroll-GetSet.js
@@ -1,78 +1,57 @@
-// const fileData = require('../css/')
 class EmployeePayrollData {
-
-    get id() {
-        return this._id;
-    }
+    get id() { return this._id }
     set id(id) {
-        this._id = id;
+        this._id = id
     }
 
-    get name() {
-        return this._name;
-    }
+    get name() { return this._name }
     set name(name) {
-        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}[" "][A-Z]{1}[a-z]{2,}$');
-        if (nameRegex.test(name)) {
-            this._name = name;
-        } else {
-            console.log("Name is Incorrect")
-        }
-    }
-    get id() {
-        return this._id
-    }
-    set id(id) {
-        this._id = id;
-    }
-    get department() {
-        return this._department;
-    }
-    set department(department) {
-        this._department = department;
-    }
-    get salary() {
-        return this._salary;
-    }
-    set salary(salary) {
-        this._salary = salary;
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$')
+        if (nameRegex.test(name))
+            this._name = name
+        else throw 'Name is Incorrect'
     }
 
-    get gender() {
-        return this._gender;
-    }
-    set gender(gender) {
-        this._gender = gender;
-    }
-
-    get note() {
-        return this._note;
-    }
-    set note(note) {
-        this._note = note;
-    }
-
-    get startDate() {
-        return this._startDate;
-    }
-    set startDate(startDate) {
-        this._startDate = startDate;
-    }
-
-    get profilePic() {
-        return this._profilePic
-    }
+    get profilePic() { return this._profilePic }
     set profilePic(profilePic) {
-        this._profilePic
+        this._profilePic = profilePic
+    }
+
+    get gender() { return this._gender }
+    set gender(gender) {
+        this._gender = gender
+    }
+
+    get departent() { return this._departent }
+    set departent(departent) {
+        this._departent = departent
+    }
+
+    get salary() { return this._salary }
+    set salary(salary) {
+        this._salary = salary
+    }
+
+    get note() { return this._note }
+    set note(note) {
+        this._note = note
+    }
+
+    get startDate() { return this._startDate }
+    set startDate(startDate) {
+        let now = new Date();
+        if (startDate > now) throw 'Start Date is Future Date!';
+        var diff = Math.abs(now.getTime() - startDate.getTime());
+        if (diff / (1000 * 60 * 60 * 24) > 30)
+            throw ' Start Date is beyount 30 days';
+        this._startDate = startDate
     }
 
     toString() {
-        const options = { year: "numeric", month: 'long', day: 'numeric' };
-        const empDate = !this.startDate.toLocaleDateString("en-US", options);
-        return "id:-" + this.id + "name :-" + this.name + "Salary :-" + this.salary + "Gender :-" + this.gender + "Department" + this.department + "startDate :-" + empDate + ",notes=" + this.note + "profilePic" + this.profilePic;
+        const options = { year: 'numeric', month: 'long', day: 'numeric' }
+        const empDate = !this.startDate ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "id=" + this.id + ", name=" + this.name + ", grnder=" + this.gender + ", profileImg=" + this.profilePic +
+            ", department=" + this.departent + ", salary=" + this.salary + ", startDate=" + empDate +
+            ", note=" + this.note
     }
-
 }
-
-let employeePayrollData = new EmployeePayrollData(1, "Dhanashri Sakharkar", 40000, "Female", 20 / 12 / 30);
-console.log(employeePayrollData.toString())
